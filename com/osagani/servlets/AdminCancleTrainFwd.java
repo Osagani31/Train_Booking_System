05880c312b40f2dae50baab2b040a414b572bff7
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-07-22 07:39:48 +0530

    Update AdminCancleTrainFwd.java

--- com/osagani/servlets/AdminCancleTrainFwd.java
+++ com/osagani/servlets/AdminCancleTrainFwd.java
@@ -1,3 +1,39 @@
 
 package com.osagani.servlets;
 
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.osagani.constant.UserRole;
+import com.osagani.utility.TrainUtil;
+
+@WebServlet("/cancletrainfwd")
+public class AdminCancleTrainFwd extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 * @param req
+	 * @param res
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		res.setContentType("text/html");
+		TrainUtil.validateUserAuthorization(req, UserRole.ADMIN);
+		RequestDispatcher rd = req.getRequestDispatcher("CancleTrain.jsp");
+		rd.forward(req, res);
+
+	}
+
+}
+
