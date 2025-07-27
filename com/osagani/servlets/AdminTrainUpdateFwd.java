bdf8f557366f9599d537dfa29c9a2a0b64931214
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-07-27 10:00:59 +0530

    Update AdminTrainUpdateFwd.java

--- com/osagani/servlets/AdminTrainUpdateFwd.java
+++ com/osagani/servlets/AdminTrainUpdateFwd.java
@@ -1,3 +1,32 @@
 
 package com.osagani.servlets;
 
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/updatetrain")
+public class AdminTrainUpdateFwd extends HttpServlet {
+
+	/**
+	 * 
+	 * @param req
+	 * @param res
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		res.setContentType("text/html");
+
+		RequestDispatcher rd = req.getRequestDispatcher("AdminUpdateTrain.jsp");
+		rd.forward(req, res);
+
+	}
+
+}
