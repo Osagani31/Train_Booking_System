4e7b4219ef942beb4cc3e22e16e0d3095902ca10
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-08-04 08:55:56 +0530

    Update TrainBwStnFwd.java

--- com/osagani/servlets/TrainBwStnFwd.java
+++ com/osagani/servlets/TrainBwStnFwd.java
@@ -1,3 +1,31 @@
 
 package com.osagani.servlets;
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.osagani.constant.UserRole;
+import com.osagani.utility.TrainUtil;
+
+@SuppressWarnings("serial")
+@WebServlet("/trainbwstnfwd")
+public class TrainBwStnFwd extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		res.setContentType("text/html");
+
+		TrainUtil.validateUserAuthorization(req, UserRole.CUSTOMER);
+
+		RequestDispatcher rd = req.getRequestDispatcher("TrainBwStn.jsp");
+		rd.forward(req, res);
+
+	}
+
+}
+
+
 
