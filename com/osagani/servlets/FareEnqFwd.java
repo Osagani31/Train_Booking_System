12da971434df0985c40c3695b2149d818b27b301
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-08-04 08:52:25 +0530

    Update FareEnqFwd.java

--- com/osagani/servlets/FareEnqFwd.java
+++ com/osagani/servlets/FareEnqFwd.java
@@ -1,2 +1,28 @@
 
 package com.osagani.servlets;
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.osagani.constant.UserRole;
+import com.osagani.utility.TrainUtil;
+
+@SuppressWarnings("serial")
+@WebServlet("/fareenqfwd")
+public class FareEnqFwd extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		res.setContentType("text/html");
+
+		TrainUtil.validateUserAuthorization(req, UserRole.CUSTOMER);
+
+		RequestDispatcher rd = req.getRequestDispatcher("Fare.jsp");
+		rd.forward(req, res);
+
+	}
+
+}
