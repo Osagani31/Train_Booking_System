e2ade8ff14d815da8182010a4cbd054011970e6b
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-08-08 08:17:49 +0530

    Update UserAvailFwd.java

--- com/osagani/servlets/UserAvailFwd.java
+++ com/osagani/servlets/UserAvailFwd.java
@@ -1,3 +1,26 @@
-
 package com.osagani.servlets;
 
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.osagani.constant.UserRole;
+import com.osagani.utility.TrainUtil;
+
+@SuppressWarnings("serial")
+@WebServlet("/useravailfwd")
+public class UserAvailFwd extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		res.setContentType("text/html");
+		TrainUtil.validateUserAuthorization(req, UserRole.CUSTOMER);
+		RequestDispatcher rd = req.getRequestDispatcher("Availability.jsp");
+		rd.forward(req, res);
+
+	}
+
+}
